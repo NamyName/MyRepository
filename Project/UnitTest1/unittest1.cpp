b9471c5fe8b1dca1cfeaaedfc90b84d6d19f2f9d
NamyName <86003339+NamyName@users.noreply.github.com>
2021-06-19 15:25:48 +0300

    Добавленны тесты для всех функций.
    Также обновлены некоторые старые, показавшие свою несостоятельность.

--- Project/UnitTest1/unittest1.cpp
+++ Project/UnitTest1/unittest1.cpp
@@ -52,6 +52,8 @@ namespace UnitTest1
 			x.src[3] = 1;
 			Assert::IsTrue(eqimpl(x, a));
 		}
+
+
 		
 		TEST_METHOD(unmergeable)
 		{
@@ -103,6 +105,12 @@ namespace UnitTest1
 			b.src[7] = 0;
 			b.src[1] = 1;
 			Assert::IsFalse(consume(a, b));
+		}
+
+		TEST_METHOD(selfconsume)
+		{
+			impl a = inttoimpl(7, 4);
+			Assert::IsTrue(consume(a, a));
 		}
 		
 		TEST_METHOD(inttoimpltotal)
@@ -156,7 +164,7 @@ namespace UnitTest1
 			Assert::AreEqual(t[2].marker, -1);
 			Assert::AreEqual(t[3].src[0], 4);
 		}
-               
+	    
 		TEST_METHOD(parse_inputcheckzero)
 		{
 			string main = "00000000";
@@ -182,14 +190,10 @@ namespace UnitTest1
 		
 		TEST_METHOD(firstfasegeneral)
 		{
-			string main = "0000000011111111";
+			string main = "0000011110011011";
 			vector<impl>t = parse_input(main, 1);
 			vector<impl>a = firstfase(t);
-			impl check;
-			check.n = 4;
-			check.src = { 1, -1, -1, -1 };
-			Assert::IsTrue(eqimpl(a[0], check));
-			Assert::AreEqual(a[1].marker, -1);
+			Assert::AreEqual(a[5].marker, -1);
 		}
 
 		TEST_METHOD(firstfaseempty)
@@ -199,6 +203,102 @@ namespace UnitTest1
 			vector<impl>a = firstfase(t);
 			Assert::AreEqual(a[0].marker, -1);
 		}
-	
+
+		TEST_METHOD(simplesimplify)
+		{
+			impl a = inttoimpl(7, 4);
+			impl b = a;
+			b.src[1] = -1;
+			impl c = a;
+			c.src[0] = 1;
+			impl breaker;
+			breaker.marker = -1;
+			vector<impl>d = { b, c, a, breaker };
+			vector<int>e = simplify(d, a);
+			Assert::IsTrue(consume(d[2], a));
+			Assert::AreEqual(e[0], 1);
+			Assert::AreEqual(e[1], 0);
+			Assert::AreEqual(e[2], 1);
+		}
+
+		TEST_METHOD(noncoreempty)
+		{
+			markedvector a;
+			vector<markedvector> b;
+			markedvector c = noncorerecursive(a, b);
+			Assert::IsTrue(c.body.empty());
+		}
+
+		TEST_METHOD(noncorefailed)
+		{
+			markedvector a;
+			markedvector c;
+			c.body = { 0 };
+			vector<markedvector> b = { c };
+			markedvector i = noncorerecursive(a, b);
+			Assert::AreEqual(i.marker, -100);
+		}
+
+		TEST_METHOD(noncoresmall)
+		{
+			markedvector a;
+			a.indexes = { 0, 1 };
+			markedvector b;
+			b.body = { 1, 0 };
+			markedvector c = noncorerecursive(a, vector<markedvector> {b, b, b, b, b, b, b, b});
+			Assert::AreEqual(c.marker, 1);
+			Assert::AreEqual( c.body[0], 0 );
+		}
+		
+		TEST_METHOD(noncoregeneral)
+		{
+			markedvector a;
+			a.indexes = { 0, 1, 2, 3};
+			markedvector c;
+			c.body = { 0, 0, 1, 1 };
+			markedvector d;
+			d.body = { 0, 1, 0, 1 };
+			markedvector e;
+			e.body = { 1, 1, 0, 0 };
+			vector<markedvector> j = { c, d, e };
+			markedvector b = noncorerecursive(a, j);
+			Assert::AreEqual(b.marker, 2);
+		}
+
+		TEST_METHOD(secondfasesimplebig)
+		{
+			vector<impl> h = parse_input("1111111111111111111111111111111111111111111111111111111111111111", 1);
+			vector<impl>b = firstfase(h);
+			vector<impl>a = secondfase(h, b);
+			Assert::AreEqual(b[1].marker, -1);
+			Assert::AreEqual(b[0].src[1], -1);
+			Assert::AreEqual(a[1].marker, -1);
+			Assert::AreEqual(a[0].src[1], -1);
+		}
+
+		TEST_METHOD(realfulltest1)
+		{
+			vector<impl> h = parse_input("0000011110011011", 1);
+			vector<impl>b = firstfase(h);
+			vector<impl>a = secondfase(h, b);
+			Assert::AreEqual(a[4].marker, -1);
+		}
+
+		TEST_METHOD(realfulltest2)
+		{
+			vector<impl> h = parse_input("1111011000100111", 1);
+			vector<impl>a = firstfase(h);
+			vector<impl>b = secondfase(h, a);
+			Assert::AreEqual(b[4].marker, -1);
+		}
+
+		TEST_METHOD(realfulltest3)
+		{
+			vector<impl> h = parse_input("0000111001111000", 1);
+			vector<impl>a = firstfase(h);
+			vector<impl>b = secondfase(h, a);
+			Assert::AreEqual(b[5].marker, -1);
+		}
+
 	};
 }
